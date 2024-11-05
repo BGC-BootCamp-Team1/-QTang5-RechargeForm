59e1992d2ce3b03e09f50bd77ebe6d90da880065
tangqi <qtang5@slb.com>
2024-11-05 20:05:31 +0800

    feat: add recharge-form component

---
+++ src/app/recharge-form/recharge-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recharge-form',
+  templateUrl: './recharge-form.component.html',
+  styleUrl: './recharge-form.component.css'
+})
+export class RechargeFormComponent {
+
+}
