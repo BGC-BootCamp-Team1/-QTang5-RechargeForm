96bc05074b4f384ea518d3307fb3bf289e5f705e
tangqi <qtang5@slb.com>
2024-11-05 20:04:33 +0800

    chore: update app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PhoneNumberModule } from './phone-number/phone-number.module';
+import { RechargeFormComponent } from './recharge-form/recharge-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RechargeFormComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PhoneNumberModule
   ],
   providers: [],
   bootstrap: [AppComponent]
